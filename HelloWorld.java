a2c541b087dbee224f44210e3cdc1fe4eb94c675
MariaBamundo <mr26bamu@siena.edu>
2017-10-12 17:51:42 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author Maria Bamundo
+ * @version 10/12/17
+ */
+public class HelloWorld
+{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
